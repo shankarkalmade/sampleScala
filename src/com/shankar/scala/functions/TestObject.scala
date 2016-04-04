90a2c65927366b83cef47536c87a94977cc57dcc
shankarkalmade <shankar.kalmade@gmail.com>
2016-04-04 13:51:04 +0530

    test class created

---
+++ src/com/shankar/scala/functions/TestObject.scala
@@ -0,0 +1,11 @@
+package com.shankar.scala.functions
+
+object TestObject {
+  
+  def main(args: Array[String]): Unit = {
+    
+    println("Started with functions and closure  chapter")
+    
+  }
+  
+}
